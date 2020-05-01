74db247f87b922d6e7084525763ef6b6e89c5170
sessegolo <rodrigo.pimpao94@gmail.com>
2020-05-01 18:23:36 -0300

    Add CharData class.

---
+++ src/chardata.cpp
@@ -0,0 +1,25 @@
+#include "chardata.h"
+
+CharData::CharData() : m_account(), m_password(), m_charName()
+{
+}
+
+CharData::CharData(const QString &account, const QString &password, const QString &charName) :
+    m_account(account), m_password(password), m_charName(charName)
+{
+}
+
+void CharData::setAccount(const QString &account)
+{
+    m_account = account;
+}
+
+void CharData::setPassword(const QString &password)
+{
+    m_password = password;
+}
+
+void CharData::setCharName(const QString &charName)
+{
+    m_charName = charName;
+}
---
+++ src/chardata.h
@@ -0,0 +1,25 @@
+#ifndef CHARDATA_H
+#define CHARDATA_H
+#include <QString>
+#include <QStringList>
+
+class CharData
+{
+public:
+    CharData();
+    CharData(const QString &account, const QString &password, const QString &charName);
+
+    QString getAccount() const { return m_account; }
+    QString getPassword() const { return m_password; }
+    QString getCharName() const { return m_charName; }
+
+    void setAccount(const QString& account);
+    void setPassword(const QString& password);
+    void setCharName(const QString& charName);
+private:
+    QString m_account;
+    QString m_password;
+    QString m_charName;
+};
+
+#endif // CHARDATA_H
